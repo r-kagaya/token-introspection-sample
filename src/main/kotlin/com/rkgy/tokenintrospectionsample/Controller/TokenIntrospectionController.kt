d7397c5fe26113310b9eab05ed77e5f04cc71522
加賀谷諒 <ryokagaya0624@gmail.com>
2019-08-23 00:43:52 +0900

    fix endpoint

--- src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
+++ src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
@@ -4,15 +4,12 @@ import com.rkgy.tokenintrospectionsample.Service.TokenIntroscpectionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
-@Controller("token/introspection")
+@RestController
 class TokenIntrospectionController @Autowired constructor(private val tokenIntroscpectionService: TokenIntroscpectionService) {
 
-    @PostMapping
+    @PostMapping("token/introspection")
     fun index(@RequestHeader(name = "Content-Type", required = true) contentType: String, @RequestParam(name = "token") token: String, @RequestParam(name = "token" +
             "TypeHint", required = false) tokenTypeHint: String) : ResponseEntity<String> {
         if (!contentType.contains("application/x-www-form-urlencoded")) {
