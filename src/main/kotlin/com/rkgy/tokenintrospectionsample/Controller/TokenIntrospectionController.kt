e848359ff2be303dcc85de8fa8d9fdbd7c3d8f92
加賀谷諒 <ryokagaya0624@gmail.com>
2019-08-23 10:54:52 +0900

    fix request body

--- src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
+++ src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.*
 class TokenIntrospectionController @Autowired constructor(private val tokenIntroscpectionService: TokenIntroscpectionService) {
 
     @PostMapping("token/introspection")
-    fun index(@RequestHeader(name = "Content-Type", required = true) contentType: String, @RequestParam(name = "token") token: String, @RequestParam(name = "token" +
-            "TypeHint", required = false) tokenTypeHint: String) : ResponseEntity<String> {
+    fun index(
+            @RequestHeader(name = "Content-Type", required = true) contentType: String,
+            @RequestParam(name = "token") token: String, 
+            @RequestParam(name = "tokenTypeHint", required = false) tokenTypeHint: String?): ResponseEntity<String> {
+        
         if (!contentType.contains("application/x-www-form-urlencoded")) {
             return ResponseEntity.badRequest().body("Content-Type " + contentType + " not supported")
         }
-        return tokenIntroscpectionService.fetchTokenInfo(token = token, tokenTypeHint = tokenTypeHint)
+        return tokenIntroscpectionService.fetchTokenInfo(token = token, tokenTypeHint = tokenTypeHint ?: "")
     }
 }
