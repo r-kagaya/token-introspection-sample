4e175d931b64832bd60dc99f08b6629894ae85a4
加賀谷諒 <ryokagaya0624@gmail.com>
2019-08-25 23:58:21 +0900

    fix authorization header

--- src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
+++ src/main/kotlin/com/rkgy/tokenintrospectionsample/Controller/TokenIntrospectionController.kt
@@ -12,12 +12,24 @@ class TokenIntrospectionController @Autowired constructor(private val tokenIntro
     @PostMapping("token/introspection")
     fun index(
             @RequestHeader(name = "Content-Type", required = true) contentType: String,
+            @RequestHeader(name = "Authorization", required = true) authorizationHeader: String,
             @RequestParam(name = "token") token: String, 
             @RequestParam(name = "tokenTypeHint", required = false) tokenTypeHint: String?): ResponseEntity<String> {
         
         if (!contentType.contains("application/x-www-form-urlencoded")) {
             return ResponseEntity.badRequest().body("Content-Type " + contentType + " not supported")
         }
+
+        if (!authorizationHeader.contains(" ")) {
+            return ResponseEntity.badRequest().body("Authorization Header is invalid")
+        }
+        
+        val (authorizationType, authorizationValue) = authorizationHeader.split(" ")
+        
+        if (authorizationType != "Basic" || authorizationValue.isEmpty()) {
+            return ResponseEntity.badRequest().body("Authorization Header is invalid")
+        }
+
         return tokenIntroscpectionService.fetchTokenInfo(token = token, tokenTypeHint = tokenTypeHint ?: "")
     }
 }
